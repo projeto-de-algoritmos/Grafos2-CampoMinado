c49b40b49f6153abcb8ce3403a78211f05be2f78
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-03-07 01:32:56 -0300

    Connecting and applying search for connected person or bombs
    Co-authored-by: Bruno Dantas <oliveiradantas96@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import fields from './Graph/board';
+import { useEffect, useState } from 'react';
+import Graph from './Graph';
 
 function App() {
+  const [board, setBoard] = useState({});
+
+  const createGraph = () => {
+    const graph = new Graph();
+
+    Object.keys(fields).forEach(field => {
+      graph.addNode(field);
+    });
+
+    Object.keys(fields).forEach(field => {
+      fields[field].connected.forEach(connection => {
+        graph.addEdge(field, connection);
+      });
+    });
+
+    return graph;
+  }
+  
+  useEffect(() => {
+    const grafo = createGraph();
+    setBoard(grafo);
+  }, []);
+
+  const showBombs = () => {
+    Object.keys(fields).forEach(field => {
+      if(fields[field].bomb){
+        const myDiv = document.getElementById(`i${field}`);
+        myDiv.style.backgroundImage = ;
+      }
+    })
+  }
+
+  const explodes = (e, field) => {
+    e.preventDefault();
+    board.BFS(field, (currentValue) => {
+      if(fields[field].bomb){
+        showBombs();
+        return true;
+      }
+      else{
+        console.log("eh pessoa");
+        return false;
+      }
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Campo <span id="red">Minado</span></h1>
+      <p>
+        Campo minado de componentes fortemente conectados!!!
+        É um pouco mais dificil pelo fato de não mostrar onde estão as bombas e nem quantas tem perto.
+        Como foi dito, existem diversos componentes fortemente conectados dentro do <span id="risk">grafo</span> campo minado.
+        Como é de se esperar, as bombas também são componentes fortemente conectados. Boa sorte!
+      </p>
+      <div className="board">
+        {Object.keys(fields).map((field) => (
+          <div className="field" id={`i${field}`} key={field} onClick={(e) => explodes(e, field)}/>
+        ))}
+        </div>
+      </div>
   );
 }
 
